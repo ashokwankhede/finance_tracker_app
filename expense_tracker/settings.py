e2993a1a05996ad4b43eb30a1e3f3822bedfb9ab
a.wankhede <a.wankhede@rnt.ai>
2025-01-18 12:22:19 +0530

    .

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -75,27 +75,18 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "tracker_db",
-#         "USER": "postgres",
-#         "PASSWORD": "Admin@123",
-#         "HOST": "localhost",
-#         "PORT": "5432"
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'mydatabase'),
-        'USER': os.getenv('DB_USER', 'myuser'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tracker_db",
+        "USER": "app_user",
+        "PASSWORD": "admin@123",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
